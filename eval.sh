5b2c2b3943c8097c36ffd8a142c1caa49a6a24b3
xiulinyang <xiulin.yang.compling@gmail.com>
2025-12-05 13:32:36 -0500

    update eval

--- eval.sh
+++ eval.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python benchmark_eval.py xiulinyang/GPT2_no_function_53 blimp/no_function_blimp
-python benchmark_eval.py xiulinyang/GPT2_no_function_42 blimp/no_function_blimp
-python benchmark_eval.py xiulinyang/GPT2_no_function_67 blimp/no_function_blimp
+python benchmark_eval.py xiulinyang/GPT2_no_function_53 no_function_blimp
+python benchmark_eval.py xiulinyang/GPT2_no_function_42 no_function_blimp
+python benchmark_eval.py xiulinyang/GPT2_no_function_67 no_function_blimp
 
 
-python benchmark_eval.py xiulinyang/GPT2_five_function_53 blimp/five_function_blimp
-python benchmark_eval.py xiulinyang/GPT2_five_function_42 blimp/five_function_blimp
-python benchmark_eval.py xiulinyang/GPT2_five_function_67 blimp/five_function_blimp
+python benchmark_eval.py xiulinyang/GPT2_five_function_53 five_function_blimp
+python benchmark_eval.py xiulinyang/GPT2_five_function_42 five_function_blimp
+python benchmark_eval.py xiulinyang/GPT2_five_function_67 five_function_blimp
 
