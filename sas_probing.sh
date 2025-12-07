a5633f29f826b15be461b871cfa36167f73f1428
xiulinyang <xiulin.yang.compling@gmail.com>
2025-12-07 13:48:42 +0100

    udpate results

--- sas_probing.sh
+++ sas_probing.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+python src/clm/evaluation/sas_probe.py -d blimp/five_function_blimp -m xiulinyang/GPT2_five_function_53 -o sas_prob/
+python src/clm/evaluation/sas_head.py -m xiulinyang/GPT2_five_function_53 -f five_function
+
+
 python src/clm/evaluation/sas_probe.py -d blimp/natural_function_blimp -m xiulinyang/GPT2_natural_function_53 -o sas_prob/
 python src/clm/evaluation/sas_head.py -m xiulinyang/GPT2_natural_function_53 -f natural_function
